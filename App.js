3bfff50bbb513efc7df705efb5065cf4af9bb972
Andrew Bulat <andrii.bulat@gmail.com>
2024-03-22 22:34:55 +0000

    Add setup for ably v2 with react native export fix

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Ably from 'ably';
 
 export default function App() {
+  useEffect(() => {
+    console.log('Hello World, from React Native!');
+
+    const realtime = new Ably.Realtime({
+      key: 'api_key',
+    });
+
+    const channel = realtime.channels.get('someChannel');
+
+    channel
+      .attach()
+      .then(() => {
+        console.log('Attached to channel');
+
+        channel.subscribe((message) => {
+          console.log('Got message from Ably: ', message);
+        });
+
+        return channel.publish('someName', { foo: 'bar' });
+      })
+      .then(() => console.log('Published to Ably'))
+      .catch((error) => console.log('Caught error: ', error));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
